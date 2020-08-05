994ee76854b4c54ea324a14eba3e6cd79b6cd7a7
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-08-05 14:45:28 +0200

    Fixes ALCHEMY upload path

--- ALCHEMY/source/docker-entrypoint.sh
+++ ALCHEMY/source/docker-entrypoint.sh
@@ -4,4 +4,4 @@ cd /root/eval
 stack setup
 # executable to build (alchemy-arithmetic) is defined in stack.yaml
 stack build --exec alchemy-arithmetic > alchemy_result_arithmetic.txt
-aws s3 cp alchemy_result_arithmetic.txt s3://sok-repository-eval-benchmarks
+aws s3 cp alchemy_result_arithmetic.txt ${S3_URL}/${S3_FOLDER}/ALCHEMY/
