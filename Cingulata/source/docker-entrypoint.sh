5ebdcb8f93ec9a14bf29f845c1440bb6b743ca31
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-08-14 17:24:42 +0200

    Adds docker-entrypoint for Cingulata

---
+++ Cingulata/source/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# run cardio in Cingulata for both the ABC-optimized circuit and the unoptimized
+# circuit
+cd /cingu/build/eval/cardio
+./run.sh
+aws s3 cp cingulata_cardio_optimized.csv ${S3_URL}/${S3_FOLDER}/Cingulata/
+aws s3 cp cingulata_cardio_unoptimized.csv ${S3_URL}/${S3_FOLDER}/Cingulata-Optimized/
