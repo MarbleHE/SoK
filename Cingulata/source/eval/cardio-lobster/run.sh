fdaf944c39795b40e932c504ebd4446691635a6f
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-08-26 16:56:23 +0200

    Fixes input for Cardio Lobster
    We encrypted three ciphertexts more than required.

--- Cingulata/source/eval/cardio-lobster/run.sh
+++ Cingulata/source/eval/cardio-lobster/run.sh
@@ -56,12 +56,13 @@ do
   # echo "Input formatting & encryption"
   NR_THREADS=1
   KS=(241 210 225 219 92 43 197)
-  # these are the same inputs as used by Lobster
-  INPUTS=(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0)
+  # These are the same inputs as used by Lobster.
+  INPUT_NAME=(i:10 i:100 i:101 i:102 i:103 i:104 i:105 i:106 i:107 i:108 i:109 i:11 i:110 i:111 i:112 i:113 i:12 i:13 i:14 i:15 i:16 i:17 i:18 i:19 i:20 i:21 i:22 i:23 i:24 i:25 i:26 i:27 i:28 i:29 i:30 i:31 i:32 i:33 i:34 i:35 i:36 i:37 i:38 i:39 i:40 i:41 i:42 i:43 i:44 i:45 i:46 i:47 i:48 i:49 i:5 i:50 i:51 i:52 i:53 i:54 i:55 i:56 i:57 i:6 i:61 i:62 i:63 i:64 i:65 i:66 i:67 i:68 i:69 i:7 i:70 i:71 i:72 i:73 i:74 i:75 i:76 i:77 i:78 i:79 i:8 i:80 i:81 i:82 i:83 i:84 i:85 i:86 i:87 i:88 i:89 i:9 i:90 i:91 i:92 i:93 i:94 i:95 i:96 i:97 i:98 i:99)
+  INPUT_VALUE=(0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
 
   # Encrypt client data
-  for (( i = 0; i < ${#INPUTS[@]}; i++ )); do
-    $APPS_DIR/encrypt --threads $NR_THREADS `$APPS_DIR/helper --bit-cnt 1 --prefix "input/i:"$(($i + 5)) ${INPUTS[i]}`
+  for (( i = 0; i < ${#INPUT_NAME[@]}; i++ )); do
+    $APPS_DIR/encrypt --threads $NR_THREADS `$APPS_DIR/helper --bit-cnt 1 --prefix "input/${INPUT_NAME[i]}" ${INPUT_VALUE[i]}`
   done
   # the helper tools creates a file in format <prefix><bit-position><suffix>
   # as we do not need the bit position (always 0), we need to remove it here from the filename
