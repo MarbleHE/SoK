f98d24b23101b15a3b027ba6fa8bb59db8f76a5f
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-08-26 17:38:42 +0200

    Adds Cardio Multistart with optimal FHE params

--- Cingulata/source/docker-entrypoint.sh
+++ Cingulata/source/docker-entrypoint.sh
@@ -6,3 +6,24 @@ cd /cingu/eval/cardio
 ./run.sh
 aws s3 cp cingulata_cardio_optimized.csv ${S3_URL}/${S3_FOLDER}/Cingulata/
 aws s3 cp cingulata_cardio_unoptimized.csv ${S3_URL}/${S3_FOLDER}/Cingulata-UNOPT/
+
+# run cardio in Cingulata with circuit optimized by Lobster and same parameters as Cingulata uses
+cd /cingu/eval/cardio-lobster
+./run.sh
+aws s3 cp cingulata_cardio_lobster.csv ${S3_URL}/${S3_FOLDER}/Lobster/
+
+# run cardio in Cingulata with circuit optimized by Lobster and parameters determined by CinguParam
+cd /cingu/eval/cardio-lobster-optimal-params
+./run.sh
+aws s3 cp cingulata_cardio_lobster_optimal.csv ${S3_URL}/${S3_FOLDER}/Lobster-OPT-PARAMS/
+
+# run cardio in Cingulata with circuit optimized by Lobster and parameters determined by CinguParam
+cd /cingu/eval/cardio-multistart
+./run.sh
+aws s3 cp cingulata_cardio_multistart.csv ${S3_URL}/${S3_FOLDER}/Cingulata-MultiStart/
+
+# run cardio in Cingulata with circuit optimized by Lobster and parameters determined by CinguParam
+cd /cingu/eval/cardio-multistart-optimal-params
+./run.sh
+aws s3 cp cingulata_cardio_multistart_optimal.csv ${S3_URL}/${S3_FOLDER}/Cingulata-MultiStart-OPT-PARAMS/
+
