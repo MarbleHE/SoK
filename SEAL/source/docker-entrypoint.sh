031201f6e60eac2106771be9feae0558900ee5de
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-08-27 18:56:22 +0200

    Updates NN-CKKS-batched file upload

--- SEAL/source/docker-entrypoint.sh
+++ SEAL/source/docker-entrypoint.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+upload_files() {
+    TARGET_DIR=$1
+    shift
+    for item in "$@"; do
+        aws s3 cp $item ${S3_URL}/${S3_FOLDER}/${TARGET_DIR}/
+    done  
+}
+
 EVAL_BUILD_DIR=/root/eval/build
 cd $EVAL_BUILD_DIR || exit
 
 # cardio BFV
 ./run-cardio-bfv.sh
-aws s3 cp seal_bfv_cardio.csv ${S3_URL}/${S3_FOLDER}/SEAL-BFV/
-aws s3 cp fhe_parameters.txt ${S3_URL}/${S3_FOLDER}/SEAL-BFV/
+upload_files SEAL-BFV seal_bfv_cardio.csv fhe_parameters.txt
 
-# cardio BFV
+# cardio BFV batched
 ./run-cardio-bfv-batched.sh
-aws s3 cp seal_batched_bfv_cardio.csv ${S3_URL}/${S3_FOLDER}/SEAL-BFV-Batched/
-aws s3 cp fhe_parameters.txt ${S3_URL}/${S3_FOLDER}/SEAL-BFV-Batched/
+upload_files SEAL-BFV-Batched seal_batched_bfv_cardio.csv fhe_parameters.txt
 
 # cardio CKKS batched
 ./run-cardio-ckks-batched.sh
-aws s3 cp seal_batched_ckks_cardio.csv ${S3_URL}/${S3_FOLDER}/SEAL-CKKS-Batched/
-aws s3 cp fhe_parameters.txt ${S3_URL}/${S3_FOLDER}/SEAL-CKKS-Batched/
+upload_files SEAL-CKKS-Batched seal_batched_ckks_cardio.csv fhe_parameters.txt
 
-# cardio nn batched
+# NN batched
 ./run-nn-ckks-batched.sh
-aws s3 cp seal_batched_ckks_cardio.csv ${S3_URL}/${S3_FOLDER}/SEAL-CKKS-Batched/
-aws s3 cp fhe_parameters_nn.txt ${S3_URL}/${S3_FOLDER}/SEAL-CKKS-Batched/
+upload_files SEAL-CKKS-Batched seal_batched_nn.csv fhe_parameters.txt
