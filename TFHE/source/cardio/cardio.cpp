89fd695fb486d81abe2624fa54f30c1273545746
Alexander Viand <alexander.viand@inf.ethz.ch>
2020-08-27 09:27:20 +0200

    Revert TFHE cardio back to ripple  carry adder
    This reverts commit 8e53105f8ff90ef93afcca9ca83850ee47ec4e53.
    This reverts commit c97dfccc6270b5f07e1243570c1630fd25f741a3.

--- TFHE/source/cardio/cardio.cpp
+++ TFHE/source/cardio/cardio.cpp
@@ -4,7 +4,6 @@
 #include <chrono>
 #include <sstream>
 #include <fstream>
-#include <iostream>
 
 typedef std::chrono::milliseconds ms;
 typedef std::chrono::high_resolution_clock Time;
@@ -56,15 +55,6 @@ int decrypt_array(const LweSample *array, int nb_size, const TFheGateBootstrappi
   return int_answer;
 }
 
-std::string decrypt_array_bits(const LweSample *array, int nb_size, const TFheGateBootstrappingSecretKeySet *key) {
-  std::string answer;
-  for (int i = 0; i < nb_size; i++) {
-    int ai = bootsSymDecrypt(&array[i], key);
-    answer = std::to_string(ai) + answer;
-  }
-  return answer;
-}
-
 void client() {
   auto t0 = Time::now();
   //generate a keyset
@@ -97,8 +87,8 @@ void client() {
   LweSample *rhs[NUM_COND];
   for (int i = 0; i < NUM_COND; ++i) {
     uint8_t flag_ptxt = 1;
-    uint8_t lhs_ptxt = 55;
-    uint8_t rhs_ptxt = 65;
+    uint8_t lhs_ptxt = 65;
+    uint8_t rhs_ptxt = 55;
     flags[i] = new_gate_bootstrapping_ciphertext_array(BIT_SIZE, params);
     lhs[i] = new_gate_bootstrapping_ciphertext_array(BIT_SIZE, params);
     rhs[i] = new_gate_bootstrapping_ciphertext_array(BIT_SIZE, params);
@@ -159,199 +149,74 @@ void client() {
 
 }
 
-/// This function implements a Sklansky prefix adder.
-/// For more information, see Section 1.2.5ff in "Design of Two Different 128-bit Adders" by Vladislav Muravin
-/// Available at https://www.semanticscholar.org/paper/e9c6e041a8ae27e17251fedcb95a835ca0ba7bd0
-/// \param[out] s a gate_bootstrapping_ciphertext_array of size nb_bits containing the sum of a and b
-/// \param[out] c_out a gate_bootstrapping_ciphertext (ptr) containing the carry out of the sum of a and b
-/// \param[in] a gate_bootstrapping_ciphertext_array of size nb_bits containing the first summand
-/// \param[in] b gate_bootstrapping_ciphertext_array of size nb_bits containing the second summand
-/// \param[in] nb_bits size (in bits) of the input and output
-/// \param[in] bk (ptr to) the bootstrapping keys
-void sklansky_adder(LweSample *s,
-                    LweSample *c_out,
-                    const LweSample *a,
-                    const LweSample *b,
-                    const int nb_bits,
-                    const TFheGateBootstrappingCloudKeySet *bk) {
+// elementary full comparator gate that is used to compare the i-th bit:
+//   input: ai and bi the i-th bit of a and b
+//          lsb_carry: the result of the comparison on the lowest bits
+//   algo: if (a==b) return lsb_carry else return b
+void compare_bit(LweSample *result,
+                 const LweSample *a,
+                 const LweSample *b,
+                 const LweSample *lsb_carry,
+                 LweSample *tmp,
+                 const TFheGateBootstrappingCloudKeySet *bk) {
+  bootsXNOR(tmp, a, b, bk);
+  bootsMUX(result, tmp, lsb_carry, a, bk);
+}
 
-  // We define propagate (p_i) and generate (g_i) bits
-  // p_i = a[i] XOR b[i] // indicates if this position will propagate a carry if one is generated at i-1
-  // g_i = a[i] AND b[i] // indicates if this position generates a new carry
+// A (very inefficiently formulated) full adder
+void full_adder(LweSample *s,
+                LweSample *c_out,
+                const LweSample *a,
+                const LweSample *b,
+                const LweSample *c_in,
+                const TFheGateBootstrappingCloudKeySet *bk) {
+  LweSample *tmp = new_gate_bootstrapping_ciphertext(bk->params);
+  bootsXOR(tmp, a, b, bk); // tmp = a XOR b
+  bootsXOR(s, tmp, c_in, bk); // s = (a XOR b) XOR c_in
 
-  // We also define P and G (for i>=k<=k)
-  // G[i][j] represents a carry being generated somewhere between bits i and j
-  // P[i][j] represents if a carry is propagated from bit j to bit i.
+  LweSample *tmp2 = new_gate_bootstrapping_ciphertext(bk->params);
+  LweSample *tmp3 = new_gate_bootstrapping_ciphertext(bk->params);
+  bootsAND(tmp2, c_in, tmp, bk); // tmp2 = c_in AND (a XOR b)
+  bootsAND(tmp3, a, b, bk); // tmp3 = a AND b
+  bootsOR(c_out, tmp2, tmp3, bk); // c_out = (a AND b) OR (c_in AND (a XOR b))
 
-  // Create P and G arrays and initialize to zero
-  LweSample *P[nb_bits];
-  LweSample *G[nb_bits];
-  for (int i = 0; i < nb_bits; ++i) {
-    P[i] = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
-    G[i] = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
-    for (int j = 0; j < nb_bits; ++j) {
-      bootsCONSTANT(&P[i][j], 0, bk);
-      bootsCONSTANT(&G[i][j], 0, bk);
-    }
-  }
-
-  // pre_computation of p_i and g_i bits
-  // These are stored into P[i][i] and G[i][i], respectively
-  for (int i = 0; i < nb_bits; ++i) {
-    bootsXOR(&P[i][i], &a[i], &b[i], bk);
-  }
-  for (int i = 0; i < nb_bits - 1; ++i) {
-    bootsAND(&G[i][i], &a[i], &b[i], bk);
-  }
-
-  // for each level
-  int num_steps = (nb_bits > 1) ? (int) std::floor(std::log2((double) nb_bits - 1)) + 1 : 0;
-  for (int step = 1; step <= num_steps; ++step) {
-    int row = 0;
-    int col = 0;
-    int k = 0;
-    // shift row
-    row += (int) std::pow(2, step - 1);
-    // do while the size of enter is not reach
-    while (row < nb_bits - 1) {
-      col = (int) std::floor(row/std::pow(2, step))*(int) std::pow(2, step);
-      for (size_t i = 0; i < (int) std::pow(2, step - 1); ++i) {
-        //evaluate_G(P, G, row, col, step);
-        k = col + (int) std::pow(2, step - 1);
-        LweSample *r = new_gate_bootstrapping_ciphertext(bk->params);
-        bootsCONSTANT(r, 0, bk);
-        bootsAND(r, &P[row][k], &G[k - 1][col], bk);
-        bootsXOR(&G[row][col], &G[row][k], r, bk);
-        delete_gate_bootstrapping_ciphertext(r);
-        if (col!=0) {
-          //evaluate_P(P, G, row, col, step);
-          k = col + (int) std::pow(2, step - 1);
-          bootsAND(&P[row][col], &P[row][k], &P[k - 1][col], bk);
-        }
-        row += 1;
-        if (row==nb_bits - 1) break;
-      }
-      row += (int) std::pow(2, step - 1);
-    }
-  }
-
-  // compute results:
-  // s[0] = P[0][0]
-  bootsCOPY(&s[0], &P[0][0], bk);
-  for (int i = 1; i < nb_bits; ++i) {
-    bootsXOR(&s[i], &P[i][i], &G[i - 1][0], bk);
-  }
-
-  // Compute c_out (carry out)
-  bootsXOR(c_out, &G[nb_bits - 1][0], &P[nb_bits - 1][nb_bits - 1], bk);
-
-  // Clean up
-  for (int i = 0; i < nb_bits; ++i) {
-    delete_gate_bootstrapping_ciphertext_array(nb_bits, G[i]);
-    delete_gate_bootstrapping_ciphertext_array(nb_bits, P[i]);
-  }
+  delete_gate_bootstrapping_ciphertext(tmp);
+  delete_gate_bootstrapping_ciphertext(tmp2);
+  delete_gate_bootstrapping_ciphertext(tmp3);
 
 }
 
-///  Compares two numbers of length nb_bits, and puts the bit (a==?b) into result
-/// \param[out] result (ptr to) gate_bootstrapping_ciphertext containing (a=?=b)
-/// \param[in] a gate_bootstrapping_ciphertext_array of size nb_bits containing the first value
-/// \param[in] b gate_bootstrapping_ciphertext_array of size nb_bits containing the second value
-/// \param[in] nb_bits size (in bits) of the input
-/// \param[in] bk (ptr to) the bootstrapping keys
-void equal(LweSample *result,
-           const LweSample *a,
-           const LweSample *b,
-           const int nb_bits,
-           const TFheGateBootstrappingCloudKeySet *bk) {
-
-  LweSample *one = new_gate_bootstrapping_ciphertext(bk->params);
-  bootsCONSTANT(one, 1, bk);
-
-  LweSample *comp = new_gate_bootstrapping_ciphertext_array(nb_bits, bk->params);
-  for (int i = 0; i < nb_bits; ++i) {
-    LweSample *tmp = new_gate_bootstrapping_ciphertext(bk->params);
-    bootsXOR(tmp, &a[i], &b[i], bk);
-    bootsXOR(&comp[i], tmp, one, bk);
-    delete_gate_bootstrapping_ciphertext(tmp);
+void ripple_carry_adder(LweSample *s,
+                        LweSample *c_out,
+                        const LweSample *a,
+                        const LweSample *b,
+                        const int nb_bits,
+                        const TFheGateBootstrappingCloudKeySet *bk) {
+  //run the elementary comparator gate n times
+  for (int i = 0; i < nb_bits; i++) {
+    full_adder(&s[i], c_out, &a[i], &b[i], c_out, bk);
   }
-  //"multvec" with log depth
-  for (int k = 1; k < nb_bits; k *= 2) {
-    for (std::size_t i = 0; i < nb_bits - k; i += 2*k) {
-      bootsAND(&comp[i], &comp[i], &comp[i + k], bk);
-    }
-  }
-  bootsCOPY(result, &comp[0], bk);
-  delete_gate_bootstrapping_ciphertext_array(nb_bits, comp);
 }
 
-///  Compares two numbers of length nb_bits, and puts the bit (a<=?b) into result
-/// \param[out] result (ptr to) gate_bootstrapping_ciphertext containing (a<=?b)
-/// \param[in] a gate_bootstrapping_ciphertext_array of size nb_bits containing the first value
-/// \param[in] b gate_bootstrapping_ciphertext_array of size nb_bits containing the second value
-/// \param[in] nb_bits size (in bits) of the input
-/// \param[in] bk (ptr to) the bootstrapping keys
+// this function compares two multibit words, and puts (a<=b) into result
 void less(LweSample *result,
           const LweSample *a,
           const LweSample *b,
           const int nb_bits,
           const TFheGateBootstrappingCloudKeySet *bk) {
-  //std::string indent(BIT_SIZE - nb_bits, '\t');
-  //fprintf(stderr, "%s less called with %d bits\n", indent.c_str(), nb_bits);
-  if (nb_bits==1) {
-    LweSample *lhs_neg = new_gate_bootstrapping_ciphertext(bk->params);
-    LweSample *one = new_gate_bootstrapping_ciphertext(bk->params);
-    bootsCONSTANT(one, 1, bk);
-    // andNY(lhs[0], rhs[0]) = !(lhs[0]) & rhs[0]
-    bootsXOR(lhs_neg, &a[0], one, bk);
-    bootsAND(result, lhs_neg, &b[0], bk);
-    delete_gate_bootstrapping_ciphertext(lhs_neg);
-    delete_gate_bootstrapping_ciphertext(one);
-    //int result_ptxt = bootsSymDecrypt(result, SECRET_KEY);
-    // fprintf(stderr, "%s recursion ended and result is %d\n", indent.c_str(), result_ptxt);
-    return;
+  LweSample *tmp = new_gate_bootstrapping_ciphertext(bk->params);
+  LweSample *carry_in = new_gate_bootstrapping_ciphertext(bk->params);
+  //initialize the tmp to 0
+  bootsCONSTANT(tmp, 0, bk);
+  bootsCONSTANT(carry_in, 0, bk);
+  //run the elementary comparator gate n times
+  for (int i = 0; i < nb_bits; i++) {
+    compare_bit(result, &a[i], &b[i], carry_in, tmp, bk);
+    bootsCOPY(carry_in, result, bk);
   }
-  const int len2 = (nb_bits >> 1);
 
-  //  lhs_l = a[0, len2) = a, nb_bits
-  //  lhs_h = a[len2-1, end) = a+len2-1, nb_bits-len2
-  //  rhs_l = b[0, len2) = b, nb_bits
-  //  rhs_h = b[len2-1, end) = b+len2-1, nb_bits-len2
-
-  int h_start = len2;
-  int h_nb_bits = std::max(nb_bits - h_start,1);
-  int l_start = 0;
-  int l_nb_bits = nb_bits - h_start;
-  LweSample *term1 = new_gate_bootstrapping_ciphertext(bk->params);
-  //lower(term1, lhs_h, rhs_h, ..., bk);
-  //fprintf(stderr, "%s calling less for h on %d, %d\n", indent.c_str(), h_start, h_nb_bits);
-  less(term1, a + h_start, b + h_start, h_nb_bits, bk);
-  //int term1_ptxt = bootsSymDecrypt(term1, SECRET_KEY);
-  //fprintf(stderr, "%s less h on %d, %d  is %d\n", indent.c_str(), h_start, h_nb_bits, term1_ptxt);
-
-  LweSample *eq = new_gate_bootstrapping_ciphertext(bk->params);
-  LweSample *ls = new_gate_bootstrapping_ciphertext(bk->params);
-  //equal(eq,lhs_h,rhs_h,l,bk);
-  equal(eq, a + len2, b + len2, nb_bits - len2, bk);
-  //int eq_ptxt = bootsSymDecrypt(eq, SECRET_KEY);
-  //fprintf(stderr, "%s equal h on %d, %d  is %d\n", indent.c_str(), len2, nb_bits - len2, eq_ptxt);
-  //less(ls,lhs_l,rhs_l,l,bk);
-  //fprintf(stderr, "%s calling less for l on %d, %d\n", indent.c_str(), l_start,l_nb_bits);
-  less(ls, a + l_start, b + l_start, l_nb_bits, bk);
-  int less_ptxt = bootsSymDecrypt(ls, SECRET_KEY);
-  //fprintf(stderr, "%s less l on %d, %d is %d\n", indent.c_str(), l_start, l_nb_bits, less_ptxt);
-
-  LweSample *term2 = new_gate_bootstrapping_ciphertext(bk->params);
-  bootsAND(term2, eq, ls, bk);
-  bootsXOR(result, term1, term2, bk);
-
-  delete_gate_bootstrapping_ciphertext(term1);
-  delete_gate_bootstrapping_ciphertext(term2);
-  delete_gate_bootstrapping_ciphertext(eq);
-  delete_gate_bootstrapping_ciphertext(ls);
-
-  //int result_ptxt = bootsSymDecrypt(result, SECRET_KEY);
-  //fprintf(stderr, "%s function call for %d ended and result is %d\n", indent.c_str(), nb_bits, result_ptxt);
+  delete_gate_bootstrapping_ciphertext(tmp);
+  delete_gate_bootstrapping_ciphertext(carry_in);
 }
 
 void cloud() {
@@ -388,24 +253,22 @@ void cloud() {
       import_gate_bootstrapping_ciphertext_fromFile(cloud_data, &rh[j], params);
   fclose(cloud_data);
 
-  // DEBUG: DECRYPT ALL THE CIPHERTEXTS
-  //  fprintf(stderr, "FLAGS:\n");
-  //  for (int i = 0; i < NUM_COND; ++i) {
-  //    int f = decrypt_array(flags[i], BIT_SIZE, SECRET_KEY);
-  //    fprintf(stderr, "%d\n", f);
-  //  }
-  //  fprintf(stderr, "LHS:\n");
-  //  for (int i = 0; i < NUM_COND; ++i) {
-  //    int f = decrypt_array(lhs[i], BIT_SIZE, SECRET_KEY);
-  //    std::string s = decrypt_array_bits(lhs[i], BIT_SIZE, SECRET_KEY);
-  //    fprintf(stderr, "%d (%s)\n", f, s.c_str());
-  //  }
-  //  fprintf(stderr, "RHS:\n");
-  //  for (int i = 0; i < NUM_COND; ++i) {
-  //    int f = decrypt_array(rhs[i], BIT_SIZE, SECRET_KEY);
-  //    std::string s = decrypt_array_bits(rhs[i], BIT_SIZE, SECRET_KEY);
-  //    fprintf(stderr, "%d (%s)\n", f, s.c_str());
-  //  }
+//  // DEBUG: DECRYPT ALL THE CIPHERTEXTS
+//  printf("FLAGS:\n");
+//  for (int i = 0; i < NUM_COND; ++i) {
+//    int f = decrypt_array(flags[i], BIT_SIZE, SECRET_KEY);
+//    printf("%d\n", f);
+//  }
+//  printf("LHS:\n");
+//  for (int i = 0; i < NUM_COND; ++i) {
+//    int f = decrypt_array(lhs[i], BIT_SIZE, SECRET_KEY);
+//    printf("%d\n", f);
+//  }
+//  printf("RHS:\n");
+//  for (int i = 0; i < NUM_COND; ++i) {
+//    int f = decrypt_array(rhs[i], BIT_SIZE, SECRET_KEY);
+//    printf("%d\n", f);
+//  }
 
   // Calculate each condition
   LweSample *cond_results[NUM_COND];
@@ -448,7 +311,7 @@ void cloud() {
   for (auto &cond_result : cond_results) {
     //initialize the carry to 0
     bootsCONSTANT(carry, 0, bk);
-    sklansky_adder(result, carry, cond_result, result, BIT_SIZE, bk);
+    ripple_carry_adder(result, carry, cond_result, result, BIT_SIZE, bk);
   }
   delete_gate_bootstrapping_ciphertext(carry);
 
