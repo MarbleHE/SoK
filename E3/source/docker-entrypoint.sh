e8ea2f8d34d4bea2e218913b45b17b0241b0844d
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-09-02 19:59:53 +0200

    Adds cardio program impl. for E3-TFHE and E3-SEAL

--- E3/source/docker-entrypoint.sh
+++ E3/source/docker-entrypoint.sh
@@ -1,19 +1,111 @@
 #!/bin/bash
+set -euo pipefail
+shopt -s inherit_errexit
 
-E3_ROOT=/e3/src
-export MAKEFLAGS=-j`nproc`
+export E3=/e3
+export E3_SRC=/e3/src
+export MAKEFLAGS=-j$(nproc)
+
+write_circuit_config() {
+    cat >cgt.cfg <<EOL
+password = anyRandomStringToBeUsedForKeyGeneration
+@sizes = 8
+Type : circuit
+{
+    postfix = Ep
+    encryption = $1
+    sizes = 8
+}
+EOL
+}
+
+get_timestamp_ms() {
+  echo $(date +%s%3N)
+}
 
 # The workflow for building used here is described in section "manual building"
 # at https://github.com/momalab/e3/tree/master/doc#manual-build-using-cgtexe.
-PROGRAM_DIR=/e3/eval/fibo
-echo "Running demo program 'fibo' with TFHE"
-cd ${E3_ROOT}
-make cgt.exe
-./cgt.exe gen -c ${PROGRAM_DIR}/cgt.cfg -d ${PROGRAM_DIR}
-./amalgam.sh
-g++ -I./ -I${PROGRAM_DIR} cgtshared.cpp secint.cpp ${PROGRAM_DIR}/*.cpp -o ./bob.exe
-echo "Running computation..."
-./bob.exe
-echo "Decrypting result..."
-make alice USER=${PROGRAM_DIR}
-./alice.exe
+# PROGRAM_DIR=/e3/eval/chi-squared
+# echo "Running demo program 'fibo' with TFHE"
+# cd ${E3_SRC}
+# make SEAL=1 cgt.exe
+# ./cgt.exe gen -c ${PROGRAM_DIR}/cgt.cfg -d ${PROGRAM_DIR}
+# ./amalgam.sh
+# g++ -I./ -I${PROGRAM_DIR} cgtshared.cpp secint.cpp ${PROGRAM_DIR}/*.cpp -o ./bob.exe
+# echo "Running computation..."
+# ./bob.exe
+# echo "Decrypting result..."
+# make alice USER=${PROGRAM_DIR}
+# ./alice.exe
+
+###############
+# TFHE Programs
+###############
+
+# CARDIO
+export OUT_FILENAME=e3_tfhe_cardio.csv
+
+cd $E3/src && \
+    make cleanall && \
+    make TFHE=1
+
+cd $E3/eval/cardio
+echo "t_keygen,t_input_encryption,t_computation,t_decryption" > $OUT_FILENAME
+
+write_circuit_config tfhe
+cp $E3/src/e3int.h ./ 
+cp $E3/src/cgtshared.* ./
+
+START_KEYGEN_T=$( get_timestamp_ms )
+$E3/src/cgt.exe gen -r $E3/src
+END_KEYGEN_T=$( get_timestamp_ms )
+echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
+
+g++ -std=c++17 -I$E3/3p/tfhe_unx/inc/tfhe -I$E3/3p/tfhe_unx/inc/fftwa -I$E3/3p/tfhe_unx/inc/fftw3 main.cpp secint.cpp cgtshared.cpp -o bob.exe $E3/3p/tfhe_unx/target/libtfhe.a $E3/3p/tfhe_unx/target/libfftw3.a
+echo -ne "0," >> $OUT_FILENAME
+
+START_COMPUTATION_T=$( get_timestamp_ms )
+./bob.exe > output.tmp
+END_COMPUTATION_T=$( get_timestamp_ms )
+echo -ne "$((${END_COMPUTATION_T}-${START_COMPUTATION_T}))," >> $OUT_FILENAME
+
+START_DECRYPTION_T=$( get_timestamp_ms )
+echo "Result:" $($E3/src/cgt.exe dec -c cgt.cfg -f output.tmp)
+END_DECRYPTION_T=$( get_timestamp_ms )
+echo "$((${END_DECRYPTION_T}-${START_DECRYPTION_T}))" >> $OUT_FILENAME
+
+###############
+# SEAL Programs
+###############
+
+# CARDIO
+export OUT_FILENAME=e3_seal_cardio.csv
+
+cd $E3/src && \
+    make cleanall && \
+    make SEAL=1
+
+cd $E3/eval/cardio
+echo "t_keygen,t_input_encryption,t_computation,t_decryption" > $OUT_FILENAME
+
+write_circuit_config seal
+cp $E3/src/e3int.h ./
+cp $E3/src/cgtshared.* ./
+
+START_KEYGEN_T=$( get_timestamp_ms )
+$E3/src/cgt.exe gen -r $E3/src
+END_KEYGEN_T=$( get_timestamp_ms )
+echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
+
+g++ -std=c++17 -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
+echo -ne "0," >> $OUT_FILENAME
+
+START_COMPUTATION_T=$( get_timestamp_ms )
+./bob.exe > output.tmp
+END_COMPUTATION_T=$( get_timestamp_ms )
+echo -ne "$((${END_COMPUTATION_T}-${START_COMPUTATION_T}))," >> $OUT_FILENAME
+
+START_DECRYPTION_T=$( get_timestamp_ms )
+echo "Result:" $($E3/src/cgt.exe dec -c cgt.cfg -f output.tmp)
+END_DECRYPTION_T=$( get_timestamp_ms )
+echo "$((${END_DECRYPTION_T}-${START_DECRYPTION_T}))" >> $OUT_FILENAME
