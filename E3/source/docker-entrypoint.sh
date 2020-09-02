624571855c4f5e3db414838a14fa8e8794bd8f75
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-09-02 21:37:24 +0200

    Adds reptition mechanism (NUM_RUNS) to E3

--- E3/source/docker-entrypoint.sh
+++ E3/source/docker-entrypoint.sh
@@ -48,28 +48,33 @@ cd $E3/src && \
 cd $E3/eval/cardio
 echo "t_keygen,t_input_encryption,t_computation,t_decryption" > $OUT_FILENAME
 
-cp $E3/src/e3int.h ./ 
-cp $E3/src/cgtshared.* ./
+RUN=1
+while (( $RUN <= $NUM_RUNS ))
+do
+  RUN=$(( $RUN + 1))
+    cp $E3/src/e3int.h ./ 
+    cp $E3/src/cgtshared.* ./
 
-START_KEYGEN_T=$( get_timestamp_ms )
-$E3/src/cgt.exe gen -c ${CGT_FILENAME} -r $E3/src
-END_KEYGEN_T=$( get_timestamp_ms )
-echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
+    START_KEYGEN_T=$( get_timestamp_ms )
+    $E3/src/cgt.exe gen -c ${CGT_FILENAME} -r $E3/src
+    END_KEYGEN_T=$( get_timestamp_ms )
+    echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
 
-g++ -std=c++17 -I$E3/3p/tfhe_unx/inc/tfhe -I$E3/3p/tfhe_unx/inc/fftwa -I$E3/3p/tfhe_unx/inc/fftw3 main.cpp secint.cpp cgtshared.cpp -o bob.exe $E3/3p/tfhe_unx/target/libtfhe.a $E3/3p/tfhe_unx/target/libfftw3.a
-echo -ne "0," >> $OUT_FILENAME
+    g++ -std=c++17 -I$E3/3p/tfhe_unx/inc/tfhe -I$E3/3p/tfhe_unx/inc/fftwa -I$E3/3p/tfhe_unx/inc/fftw3 main.cpp secint.cpp cgtshared.cpp -o bob.exe $E3/3p/tfhe_unx/target/libtfhe.a $E3/3p/tfhe_unx/target/libfftw3.a
+    echo -ne "0," >> $OUT_FILENAME
 
-START_COMPUTATION_T=$( get_timestamp_ms )
-./bob.exe > output.tmp
-END_COMPUTATION_T=$( get_timestamp_ms )
-echo -ne "$((${END_COMPUTATION_T}-${START_COMPUTATION_T}))," >> $OUT_FILENAME
+    START_COMPUTATION_T=$( get_timestamp_ms )
+    ./bob.exe > output.tmp
+    END_COMPUTATION_T=$( get_timestamp_ms )
+    echo -ne "$((${END_COMPUTATION_T}-${START_COMPUTATION_T}))," >> $OUT_FILENAME
 
-START_DECRYPTION_T=$( get_timestamp_ms )
-echo "Result:" $($E3/src/cgt.exe dec -c ${CGT_FILENAME} -f output.tmp)
-END_DECRYPTION_T=$( get_timestamp_ms )
-echo "$((${END_DECRYPTION_T}-${START_DECRYPTION_T}))" >> $OUT_FILENAME
+    START_DECRYPTION_T=$( get_timestamp_ms )
+    echo "Result:" $($E3/src/cgt.exe dec -c ${CGT_FILENAME} -f output.tmp)
+    END_DECRYPTION_T=$( get_timestamp_ms )
+    echo "$((${END_DECRYPTION_T}-${START_DECRYPTION_T}))" >> $OUT_FILENAME
+done
 
-upload_files E3-TFHE ${OUTPUT_FILENAME}
+upload_files E3-TFHE ${OUT_FILENAME}
 
 ###############
 # SEAL Programs
@@ -86,25 +91,29 @@ cd $E3/src && \
 cd $E3/eval/cardio
 echo "t_keygen,t_input_encryption,t_computation,t_decryption" > $OUT_FILENAME
 
-cp $E3/src/e3int.h ./
-cp $E3/src/cgtshared.* ./
+RUN=1
+while (( $RUN <= $NUM_RUNS ))
+do
+    cp $E3/src/e3int.h ./
+    cp $E3/src/cgtshared.* ./
 
-START_KEYGEN_T=$( get_timestamp_ms )
-$E3/src/cgt.exe gen -c ${CGT_FILENAME} -r $E3/src
-END_KEYGEN_T=$( get_timestamp_ms )
-echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
+    START_KEYGEN_T=$( get_timestamp_ms )
+    $E3/src/cgt.exe gen -c ${CGT_FILENAME} -r $E3/src
+    END_KEYGEN_T=$( get_timestamp_ms )
+    echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
 
-g++ -std=c++17 -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
-echo -ne "0," >> $OUT_FILENAME
+    g++ -std=c++17 -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
+    echo -ne "0," >> $OUT_FILENAME
 
-START_COMPUTATION_T=$( get_timestamp_ms )
-./bob.exe > output.tmp
-END_COMPUTATION_T=$( get_timestamp_ms )
-echo -ne "$((${END_COMPUTATION_T}-${START_COMPUTATION_T}))," >> $OUT_FILENAME
+    START_COMPUTATION_T=$( get_timestamp_ms )
+    ./bob.exe > output.tmp
+    END_COMPUTATION_T=$( get_timestamp_ms )
+    echo -ne "$((${END_COMPUTATION_T}-${START_COMPUTATION_T}))," >> $OUT_FILENAME
 
-START_DECRYPTION_T=$( get_timestamp_ms )
-echo "Result:" $($E3/src/cgt.exe dec -c ${CGT_FILENAME} -f output.tmp)
-END_DECRYPTION_T=$( get_timestamp_ms )
-echo "$((${END_DECRYPTION_T}-${START_DECRYPTION_T}))" >> $OUT_FILENAME
+    START_DECRYPTION_T=$( get_timestamp_ms )
+    echo "Result:" $($E3/src/cgt.exe dec -c ${CGT_FILENAME} -f output.tmp)
+    END_DECRYPTION_T=$( get_timestamp_ms )
+    echo "$((${END_DECRYPTION_T}-${START_DECRYPTION_T}))" >> $OUT_FILENAME
+done
 
-upload_files E3-TFHE ${OUTPUT_FILENAME}
+upload_files E3-TFHE ${OUT_FILENAME}
