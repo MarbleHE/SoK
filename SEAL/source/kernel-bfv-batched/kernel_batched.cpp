ef5ae569089ff0f65f1444d100296271b8d9d0b2
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-09-09 13:28:02 +0200

    Improves kernel BFV batched

--- SEAL/source/kernel-bfv-batched/kernel_batched.cpp
+++ SEAL/source/kernel-bfv-batched/kernel_batched.cpp
@@ -1,73 +1,43 @@
 #include "kernel_batched.h"
 
-#define NUM_CTXT_SLOTS 16384
 #define IMAGE_SIZE 32
 
-void Evaluation::encryptedLaplacianSharpeningAlgorithmBatched(VecInt2D img) {
-  /// the encrypted data in this Ciphertext
-  seal::Ciphertext ciphertext;
-
-  /// the seal context, i.e. object that holds params/etc
-  static std::shared_ptr<seal::SEALContext> context;
-
-  /// secret key, also used for (more efficient) encryption (ptr for
-  /// consistency)
-  static std::unique_ptr<seal::SecretKey> secretKey;
-
-  /// public key (ptr because PublicKey() segfaults)
-  static std::unique_ptr<seal::PublicKey> publicKey;
-
-  /// keys required to rotate (ptr because GaloisKeys() segfaults)
-  static std::unique_ptr<seal::GaloisKeys> galoisKeys;
-
-  /// keys required to relinearize after multipliction (segfaulting not tested,
-  /// but ptr for consistency)
-  static std::unique_ptr<seal::RelinKeys> relinKeys;
-
-  /// Wrapper for parameters
+void Evaluation::run_kernel(VecInt2D img) {
   seal::EncryptionParameters params(seal::scheme_type::BFV);
-
   // in BFV, this degree is also the number of slots.
-  params.set_poly_modulus_degree(NUM_CTXT_SLOTS);
-
+  params.set_poly_modulus_degree(DEFAULT_NUM_SLOTS);
   // Let SEAL select a "suitable" coefficient modulus (not necessarily maximal)
-  params.set_coeff_modulus(
-      seal::CoeffModulus::BFVDefault(params.poly_modulus_degree()));
-
+  params.set_coeff_modulus(seal::CoeffModulus::BFVDefault(params.poly_modulus_degree()));
   // Let SEAL select a plaintext modulus that actually supports batching
   params.set_plain_modulus(
       seal::PlainModulus::Batching(params.poly_modulus_degree(), 20));
-
-  // Instantiate context
   context = seal::SEALContext::Create(params);
 
-  /// Helper object to create keys
-  seal::KeyGenerator keyGenerator(context);
+  /// Create keys
+  seal::KeyGenerator keygen(context);
+  secret_key = std::make_unique<seal::SecretKey>(keygen.secret_key());
+  public_key = std::make_unique<seal::PublicKey>(keygen.public_key());
+  galois_keys = std::make_unique<seal::GaloisKeys>(keygen.galois_keys_local());
+  relin_keys = std::make_unique<seal::RelinKeys>(keygen.relin_keys_local());
 
-  secretKey = std::make_unique<seal::SecretKey>(keyGenerator.secret_key());
-  publicKey = std::make_unique<seal::PublicKey>(keyGenerator.public_key());
-  galoisKeys =
-      std::make_unique<seal::GaloisKeys>(keyGenerator.galois_keys_local());
-  relinKeys =
-      std::make_unique<seal::RelinKeys>(keyGenerator.relin_keys_local());
+  // Create helper objects
+  encoder = std::make_unique<seal::BatchEncoder>(context);
+  // secret key encryptor is more efficient
+  encryptor = std::make_unique<seal::Encryptor>(context, *public_key, *secret_key);
 
-  auto encoder = seal::BatchEncoder(context);
-  auto encryptor =
-      seal::Encryptor(context, *publicKey,
-                      *secretKey);  // secret Key encryptor is more efficient
-
-  // Encrypt input
+  // Encrypt input image
   std::vector<int64_t> img_as_vec;
   img_as_vec.reserve(img.size() * img.size());
-  for (int x = 1; x < img.size() - 1; ++x) {
-    for (int y = 1; y < img.at(x).size() - 1; ++y) {
-      img_as_vec.push_back(img[x][y]);
+  for (auto row : img) {
+    for (auto elem : row) {
+      img_as_vec.push_back(elem);
     }
   }
+
   seal::Plaintext img_ptxt;
-  encoder.encode(img_as_vec, img_ptxt);
+  encoder->encode(img_as_vec, img_ptxt);
   seal::Ciphertext img_ctxt(context);
-  encryptor.encrypt_symmetric(img_ptxt, img_ctxt);
+  encryptor->encrypt_symmetric(img_ptxt, img_ctxt);
 
   // Compute sharpening filter
   auto evaluator = seal::Evaluator(context);
@@ -93,11 +63,11 @@ void Evaluation::encryptedLaplacianSharpeningAlgorithmBatched(VecInt2D img) {
     img_ctxts[i] = (i == 0)
                        ? std::move(img_ctxt)
                        : img_ctxts[0];  // move for i == 0 saves one ctxt copy
-    evaluator.rotate_rows_inplace(img_ctxts[i], (int)rotations[i], *galoisKeys);
+    evaluator.rotate_rows_inplace(img_ctxts[i], (int)rotations[i], *galois_keys);
     //    seal::Ciphertext dst(context);
-    //    evaluator.rotate_vector(img_ctxts[i], (int) rotations[i], *galoisKeys,
+    //    evaluator.rotate_vector(img_ctxts[i], (int) rotations[i], *galois_keys,
     //    dst);
-    encoder.encode(std::vector<int64_t>(img_as_vec.size(), weights[i]), w_ptxt);
+    encoder->encode(std::vector<int64_t>(img_as_vec.size(), weights[i]), w_ptxt);
     evaluator.multiply_plain_inplace(img_ctxts[i], w_ptxt);
   }
 
@@ -108,24 +78,18 @@ void Evaluation::encryptedLaplacianSharpeningAlgorithmBatched(VecInt2D img) {
 
 int main(int argc, char *argv[]) {
   std::cout << "Starting benchmark 'kernel-bfv-batched'..." << std::endl;
-  std::cout << "image_size,time_ms" << std::endl;
 
   // std::vector<int> image_sizes = { 8, 16, 32, 64, 96, 128 };
   std::vector<int> image_sizes = {8};
 
   for (auto img_size : image_sizes) {
+    // generate input image with dummy data
     std::vector<int> vec(img_size);
     std::iota(vec.begin(), vec.end(), 0);
+
+    // run kernel using FHE
     std::vector<std::vector<int>> img(img_size, vec);
-    decltype(std::chrono::high_resolution_clock::now()) t_start;
-    decltype(std::chrono::high_resolution_clock::now()) t_end;
-    decltype(std::chrono::duration_cast<std::chrono::microseconds>(
-        t_end - t_start)) duration_ms;
-    t_start = std::chrono::high_resolution_clock::now();
-    Evaluation::encryptedLaplacianSharpeningAlgorithmBatched(img);
-    t_end = std::chrono::high_resolution_clock::now();
-    duration_ms =
-        std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start);
-    std::cout << img_size << "," << duration_ms.count() << std::endl;
+    Evaluation eval;
+    eval.run_kernel(img);
   }
 }
--- SEAL/source/kernel-bfv-batched/kernel_batched.h
+++ SEAL/source/kernel-bfv-batched/kernel_batched.h
@@ -4,20 +4,37 @@
 
 #include <seal/seal.h>
 
-#include <algorithm>
 #include <chrono>
-#include <cmath>
-#include <functional>
 #include <iostream>
-#include <memory>
-#include <numeric>
-#include <random>
 #include <vector>
 
 typedef std::vector<std::vector<int>> VecInt2D;
 
+typedef std::vector<std::vector<int>> VecInt2D;
+typedef std::chrono::high_resolution_clock Time;
+typedef decltype(std::chrono::high_resolution_clock::now()) Timepoint;
+typedef long long Duration;
+typedef std::chrono::milliseconds ms;
+
+#define DEFAULT_NUM_SLOTS 16'384
+
 class Evaluation {
+ private:
+  const VecInt2D weight_matrix = {{1, 1, 1}, {1, -8, 1}, {1, 1, 1}};
+
+  std::shared_ptr<seal::SEALContext> context;
+
+  std::unique_ptr<seal::SecretKey> secret_key;
+  std::unique_ptr<seal::PublicKey> public_key;
+  std::unique_ptr<seal::GaloisKeys> galois_keys;
+  std::unique_ptr<seal::RelinKeys> relin_keys;
+
+  std::unique_ptr<seal::BatchEncoder> encoder;
+  std::unique_ptr<seal::Encryptor> encryptor;
+  std::unique_ptr<seal::Decryptor> decryptor;
+  std::unique_ptr<seal::Evaluator> evaluator;
+
  public:
-  static void encryptedLaplacianSharpeningAlgorithmBatched(VecInt2D img);
+  void run_kernel(VecInt2D img);
   int main(int argc, char *argv[]);
 };
