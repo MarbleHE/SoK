8386869c7c2b83370b0cf014e12429dc5f8554a6
Patrick Jattke <patrick.jattke@student.hpi.de>
2020-09-10 12:35:51 +0200

    Fixes a bug in the Cingulata kernel program

--- Cingulata/source/kernel/kernel.cxx
+++ Cingulata/source/kernel/kernel.cxx
@@ -46,9 +46,9 @@ int main() {
       for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
           value += weight[i + 1][j + 1] * img[x + i][y + i];
-          out[x][y] = img[x][y] - (value >> 1);
         }
       }
+      out[x][y] = (img[x][y] << 1) - value;
     }
   }
 
