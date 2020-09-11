0fd20de75861d3037989e9e6f1d37474074396f1
Patrick Jattke <pjattke@ethz.ch>
2020-09-11 12:29:09 +0200

    Adds the kernel applications to docker-entrypoint.sh

--- E3/source/docker-entrypoint.sh
+++ E3/source/docker-entrypoint.sh
@@ -51,6 +51,45 @@ cd $E3/src && \
    make cleanall && \
    make TFHE=1 -j$(nproc)
 
+echo "============================================================"
+echo "==== Running kernel-tfhe ==================================="
+echo "============================================================"
+
+export OUT_FILENAME=e3_tfhe_kernel.csv
+export CGT_FILENAME=cgt_tfhe.cfg
+
+cd $E3/eval/kernel-tfhe
+rm -f *.key secint.* *.temp *.tmp cgtshared.* *.exe e3int.* *.csv
+
+echo "t_keygen,t_input_encryption,t_computation,t_decryption" > $OUT_FILENAME
+
+RUN=1
+while (( $RUN <= $NUM_RUNS ))
+do
+   RUN=$(( $RUN + 1))
+   cp $E3/src/e3int.h ./ 
+   cp $E3/src/cgtshared.* ./
+
+   START_KEYGEN_T=$( get_timestamp_ms )
+   $E3/src/cgt.exe gen -c ${CGT_FILENAME} -r $E3/src
+   END_KEYGEN_T=$( get_timestamp_ms )
+   echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
+
+   g++ -std=c++17 -I$E3/3p/tfhe_unx/inc/tfhe -I$E3/3p/tfhe_unx/inc/fftwa -I$E3/3p/tfhe_unx/inc/fftw3 main.cpp secint.cpp cgtshared.cpp -o bob.exe $E3/3p/tfhe_unx/target/libtfhe.a $E3/3p/tfhe_unx/target/libfftw3.a
+   echo -ne "0," >> $OUT_FILENAME
+
+   START_COMPUTATION_T=$( get_timestamp_ms )
+   ./bob.exe > output.tmp
+   END_COMPUTATION_T=$( get_timestamp_ms )
+   echo -ne "$((${END_COMPUTATION_T}-${START_COMPUTATION_T}))," >> $OUT_FILENAME
+
+   START_DECRYPTION_T=$( get_timestamp_ms )
+   echo "Result:" $($E3/src/cgt.exe dec -c ${CGT_FILENAME} -f output.tmp)
+   END_DECRYPTION_T=$( get_timestamp_ms )
+   echo "$((${END_DECRYPTION_T}-${START_DECRYPTION_T}))" >> $OUT_FILENAME
+done
+
+upload_files E3-TFHE ${OUT_FILENAME}
 
 echo "============================================================"
 echo "==== Running cardio-tfhe ==================================="
@@ -137,7 +176,6 @@ cd $E3/src && \
     make cleanall && \
     make SEAL=1 -j$(nproc)
 
-# myLabel:
 echo "============================================================"
 echo "==== Running kernel-seal ==================================="
 echo "============================================================"
@@ -162,7 +200,7 @@ do
     END_KEYGEN_T=$( get_timestamp_ms )
     echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
 
-    g++ -std=c++17 -Wno-deprecated-declarations -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
+    g++ -std=c++17 -Wno-deprecated-declarations -I${E3}/src -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
     echo -ne "0," >> $OUT_FILENAME
 
     START_COMPUTATION_T=$( get_timestamp_ms )
@@ -177,9 +215,7 @@ do
 done
 
 upload_files E3-SEAL ${OUT_FILENAME}
-
-exit 0;
-
+ 
 echo "============================================================"
 echo "==== Running cardio-seal ==================================="
 echo "============================================================"
@@ -202,7 +238,7 @@ do
     END_KEYGEN_T=$( get_timestamp_ms )
     echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
 
-    g++ -std=c++17 -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
+    g++ -std=c++17 -Wno-deprecated-declarations -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
     echo -ne "0," >> $OUT_FILENAME
 
     START_COMPUTATION_T=$( get_timestamp_ms )
@@ -241,7 +277,7 @@ do
     END_KEYGEN_T=$( get_timestamp_ms )
     echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
 
-    g++ -std=c++17 -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
+    g++ -std=c++17 -Wno-deprecated-declarations -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
     echo -ne "0," >> $OUT_FILENAME
 
     START_COMPUTATION_T=$( get_timestamp_ms )
@@ -279,7 +315,7 @@ do
     END_KEYGEN_T=$( get_timestamp_ms )
     echo -ne "$((${END_KEYGEN_T}-${START_KEYGEN_T}))," >> $OUT_FILENAME
 
-    g++ -std=c++17 -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
+    g++ -std=c++17 -Wno-deprecated-declarations -I${E3}/3p/seal_unx/include main.cpp secint.cpp cgtshared.cpp -o bob.exe ${E3}/3p/seal_unx/native/libseal.a
     echo -ne "0," >> $OUT_FILENAME
 
     START_COMPUTATION_T=$( get_timestamp_ms )
