2418196a7de557019b7ac341aad236495ca3f19b
Patrick Jattke <pjattke@ethz.ch>
2020-09-11 11:31:29 +0200

    Fixes kernel in E3-SEAL BFV

--- E3/source/kernel-seal/main.cpp
+++ E3/source/kernel-seal/main.cpp
@@ -6,52 +6,53 @@
 
 using namespace std;
 
-#ifndef SZ
-#define SZ 8
-#endif
-
-// using SecureInt = TypeUint<SZ>;
-// using SecureBool = TypeBool;
 using SecureMint = TypeMint;
+
+void run_kernel(std::vector<std::vector<SecureMint>> img) {
+  const size_t image_size = img.size();
+
+  // create a copy of the input image to preserve image's border
+  std::vector<std::vector<SecureMint>> output = img;
+
+  // NOTE: We use different weights here compared to the other Kernel
+  // implementations because E3 has issues working with negative numbers (i.e.,
+  // the weight -8).
+  std::vector<std::vector<SecureMint>> wm = {
+      {_0_Ea, _0_Ea, _0_Ea}, {_0_Ea, _0_Ea, _1_Ea}, {_0_Ea, _0_Ea, _0_Ea}};
+
+  for (size_t x = 1; x < image_size - 1; x++) {
+    for (size_t y = 1; y < img.at(x).size() - 1; y++) {
+      SecureMint value = _0_Ea;
+      for (size_t j = 0; j < 3; j++) {
+        for (size_t i = 0; i < 3; i++) {
+          value = value + (wm.at(i ).at(j ) * img.at(x + i-1).at(y + j-1));
+        }
+      }
+      // output[x][y] = img[x][y] - (value / 2);
+      // modified variant as we cannot divide in FHE:
+      output[x][y] = (2 * img[x][y]) - value;
+    }
+  }
+
+  for (size_t i = 0; i < output.size(); i++) {
+    for (size_t j = 0; j < output.at(i).size(); j++) {
+      std::cout << "(" << i << "," << j << "): " << e3::decrypt(output[i][j])
+                << '\n';
+    }
+  }
+}
 
 int main() {
   // = INPUTS ===
   std::vector<std::vector<SecureMint>> img = {
       {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea},
-      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea}, 
-      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea}, 
-      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea}, 
       {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea},
-      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea}, 
-      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea}, 
-      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea}
-  };
+      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea},
+      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea},
+      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea},
+      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea},
+      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea},
+      {_0_Ea, _1_Ea, _2_Ea, _3_Ea, _4_Ea, _5_Ea, _6_Ea, _7_Ea}};
 
-  std::vector<std::vector<SecureMint>> img2 = img;
-
-  std::vector<std::vector<SecureMint>> weights = {
-      {_1_Ea, _1_Ea, _1_Ea}, 
-      {_1_Ea, _8_Ean, _1_Ea}, 
-      {_1_Ea, _1_Ea, _1_Ea}
-  };
-
-  for (size_t x = 1; x < img.size() - 1; x++) {
-    for (size_t y = 1; y < img.at(x).size() - 1; y++) {
-      SecureMint value = _0_Ea;
-      for (size_t i = -1; i < 2; i++) {
-        for (size_t j = -1; j < 2; j++) {
-          value += weights.at(i+1).at(j+1) * img.at(x + i).at(y + j);
-        }
-      }
-      // img2[x][y] = img[x][y] - (value / 2);
-      // modified variant as we cannot divide in FHE:
-      img2[x][y] = (img[x][y] << 1) - value;
-    }
-  }
-
-  for (size_t i = 0; i < img2.size(); i++) {
-    for (size_t j = 0; j < img2.at(i).size(); j++) {
-     std::cout << "(" << i << "," << j << "): " << e3::decrypt(img2[i][j]) << '\n';
-    }
-  }
+  run_kernel(img);
 }
