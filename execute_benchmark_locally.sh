c45d2e436dd293a63a4291ac53e3a487613bf43e
Patrick Jattke <pjattke@ethz.ch>
2020-09-14 12:11:07 +0200

    Fixes local benchmark testing script

--- execute_benchmark_locally.sh
+++ execute_benchmark_locally.sh
@@ -18,6 +18,8 @@ fi
 # this ensures that all tools write their results into the same folder
 S3_FOLDER_NAME=$(date +"%Y%m%d_%H%M%S")
 
+CUR_DIR=$(pwd)
+
 for TOOLNAME in "$@"
 do
   # toolname is an abbreviation for the tool that is compatible with the Docker
@@ -53,18 +55,13 @@ do
       exit 1;
   esac
 
-  TAG_NAME=eval_${TOOLNAME}
-
-  cd ${tooldir} || (echo "Could not find ${tooldir} directory! Needs to be executed from SoK root directory."; exit 1)
-
-  echo "Building eval image for ${tooldir}..."
-  docker build -t ${TAG_NAME} . && \
-  echo "Running benchmark in eval docker image..." && \
-  docker run -e S3_URL=s3://sok-repository-eval-benchmarks \
-      -e S3_FOLDER=${S3_FOLDER_NAME} \
-      -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
-      -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
-      -e AWS_DEFAULT_REGION=us-east-2 \
-      -e NUM_RUNS=1 \
-      -it ${TAG_NAME}
+  (cd ${tooldir} && \
+    echo "Building eval image for ${tooldir} and running benchmark programs ..." && \
+    docker run -d -e S3_URL=s3://sok-repository-eval-benchmarks \
+        -e S3_FOLDER=${S3_FOLDER_NAME} \
+        -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+        -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+        -e AWS_DEFAULT_REGION=us-east-2 \
+        -e NUM_RUNS=1 \
+        -it $(docker build -q .))
 done
