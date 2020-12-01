328443f91d4489f0c80405ba5a6794b90ab646fe
Patrick Jattke <pjattke@ethz.ch>
2020-12-01 12:05:47 +0100

    Fixes path in execute_benchmarks_locally script

--- scripts/dev_docker/execute_benchmark_locally.sh
+++ scripts/dev_docker/execute_benchmark_locally.sh
@@ -77,7 +77,7 @@ for TOOLNAME in "$@"; do
     ;;
   esac
 
-  (cd ${tooldir} &&
+  (cd ../../${tooldir} &&
     echo "Building eval image for ${tooldir} and running benchmark programs ..." &&
     docker run -d -e S3_URL=s3://sok-repository-eval-benchmarks \
       -e S3_FOLDER=${S3_FOLDER_NAME} \
