1f6ceaec5a7732c8adc652bbe9a40d410dae1efb
Patrick Jattke <pjattke@ethz.ch>
2020-12-15 15:51:02 +0100

    Fixes issue with CSV file parsing and S3 root path

--- scripts/paper_plots/s3_utils.py
+++ scripts/paper_plots/s3_utils.py
@@ -61,10 +61,13 @@ def get_labels_data_from_s3(name_filter: str, root_folder: str = None) -> (List[
     # get the folder with the most recent timestamp (e.g., 20200729_0949/)
     if root_folder is None:
         root_folder = get_most_recent_folder_from_s3_bucket()
+    elif not root_folder.endswith('/'):
+        root_folder += '/'
+
     # get all subfolders - this corresponds to the benchmarked tools (e.g., Cingulata, SEAL)
     tool_folders_path = get_folder_in_s3_path(root_folder)
     for tp in tool_folders_path:
-        # get path to all CSV files that have 'nn' in its name
+        # get path to all CSV files that have 'name_filter' in its name
         s3_urls = get_csv_files_in_s3_path(tp, name_filter, get_s3_url=True)
         # throw an error if there is more than one CSV with 'cardio' in the folder as each benchmark run should exactly
         # produce one CSV file per program
@@ -83,7 +86,7 @@ def get_labels_data_from_s3(name_filter: str, root_folder: str = None) -> (List[
         # use the tool's name as label for the plot
         labels.append(tool_name)
         # read the CSV data from S3
-        data.append(pd.read_csv(s3_urls[0]))
+        data.append(pd.read_csv(s3_urls[0], delimiter=','))
 
     # call the plot
     if len(labels) == 0:
