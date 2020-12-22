c9de8c0aedcc93292442d09eca3a200d4c24bd3a
Patrick Jattke <pjattke@ethz.ch>
2020-12-22 15:15:01 +0100

    Adds S3 upload to PALISADE's docker-entrypoint.sh

--- PALISADE/source/docker-entrypoint.sh
+++ PALISADE/source/docker-entrypoint.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Run microbenchmark
+function upload_files() {
+    TARGET_DIR=$1
+    shift
+    for item in "$@"; do
+        aws s3 cp $item ${S3_URL}/${S3_FOLDER}/${TARGET_DIR}/
+    done  
+}
+
+# Build benchmark programs
 cd /root/eval \
     && mkdir build \
     && cd build \
     && cmake .. \
-    && make \
-    && ./microbenchmark
+    && make
 
+# Run microbenchmark
+./microbenchmark
+export OUTPUT_FILENAME=palisade_microbenchmark.csv
+upload_files PALISADE-BFV ${OUTPUT_FILENAME} fhe_parameters_microbenchmark.txt
