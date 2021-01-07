11ab54735cef5c0010a88502cc85cfd0ade08af0
Alexander Viand <alexander.viand@inf.ethz.ch>
2021-01-07 15:00:07 +0100

    fixing one more typo in TFHE scripts

--- TFHE/source/docker-entrypoint.sh
+++ TFHE/source/docker-entrypoint.sh
@@ -11,15 +11,15 @@ upload_files() {
 EVAL_BUILD_DIR=/root/eval/build
 cd $EVAL_BUILD_DIR || exit
 
-# Cardio Naive
-export OUTPUT_FILENAME=tfhe_cardio_naive.csv
-./run_cardio_naive.sh
-upload_files TFHE-Naive ${OUTPUT_FILENAME}
-
 # Cardio Opt
 export OUTPUT_FILENAME=tfhe_cardio_opt.csv
 ./run_cardio_opt.sh
 upload_files TFHE-Opt ${OUTPUT_FILENAME}
+
+# Cardio Naive
+export OUTPUT_FILENAME=tfhe_cardio_naive.csv
+./run_cardio_naive.sh
+upload_files TFHE-Naive ${OUTPUT_FILENAME}
 
 # Chi-Squared Naive
 export OUTPUT_FILENAME=tfhe_chi_squared_naive.csv
